9dd8749ad2c587f6840606025f828b22d611c451
daffadon <daffaputranarendra9@gmail.com>
2025-07-11 11:00:59 +0700

    refactor(server): change server run strategy

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/dropboks/file-service/cmd/bootstrap"
 	"github.com/dropboks/file-service/cmd/server"
 	"github.com/spf13/viper"
@@ -8,12 +13,29 @@ import (
 
 func main() {
 	container := bootstrap.Run()
-	serverReady := make(chan bool)
-	grpcServer := server.GRPCServer{
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	grpcServerReady := make(chan bool)
+	grpcServerDone := make(chan struct{})
+	grpcServer := &server.GRPCServer{
 		Container:   container,
-		ServerReady: serverReady,
-		Address:     ":" + viper.GetString("app.port"),
+		ServerReady: grpcServerReady,
+		Address:     ":" + viper.GetString("app.grpc.port"),
 	}
-	grpcServer.Run()
-	<-serverReady
+	go func() {
+		grpcServer.Run(ctx)
+		close(grpcServerDone)
+	}()
+	<-grpcServerReady
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGABRT, syscall.SIGTERM)
+
+	<-sig
+	cancel()
+
+	<-grpcServerDone
+
 }
--- cmd/server/grpc_server.go
+++ cmd/server/grpc_server.go
@@ -4,17 +4,13 @@ import (
 	"context"
 	"log"
 	"net"
-	"os"
-	"os/signal"
-	"syscall"
+	"time"
 
 	"github.com/dropboks/file-service/internal/domain/handler"
 	"github.com/dropboks/file-service/internal/domain/service"
 	"github.com/dropboks/file-service/internal/infrastructure/storage"
 	"github.com/dropboks/file-service/pkg/constant"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog"
-	"github.com/spf13/viper"
 	"go.uber.org/dig"
 	"google.golang.org/grpc"
 )
@@ -25,16 +21,13 @@ type GRPCServer struct {
 	Address     string
 }
 
-func (s *GRPCServer) Run() {
+func (s *GRPCServer) Run(ctx context.Context) {
 	err := s.Container.Invoke(func(
 		grpcServer *grpc.Server,
 		logger zerolog.Logger,
-		db *pgxpool.Pool,
 		svc service.UserService,
 		miniio *storage.MinioStorage,
 	) {
-		defer db.Close()
-		ctx := context.Background()
 		err := miniio.InitBucket(ctx, constant.APP_BUCKET)
 		if err != nil {
 			log.Fatalf("Failed to init bucket: %v", err)
@@ -51,14 +44,20 @@ func (s *GRPCServer) Run() {
 				logger.Fatal().Msgf("gRPC server error: %v", serveErr)
 			}
 		}()
-		logger.Info().Msg("gRPC server running in port :" + viper.GetString("app.port"))
 		if s.ServerReady != nil {
-			s.ServerReady <- true
+			for range 50 {
+				conn, err := net.DialTimeout("tcp", s.Address, 100*time.Millisecond)
+				if err == nil {
+					conn.Close()
+					s.ServerReady <- true
+					break
+				}
+				time.Sleep(100 * time.Millisecond)
+			}
 		}
+		logger.Info().Msg("gRPC server running in port " + s.Address)
 
-		quit := make(chan os.Signal, 1)
-		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-		<-quit
+		<-ctx.Done()
 		logger.Info().Msg("Shutting down gRPC server...")
 		grpcServer.GracefulStop()
 		logger.Info().Msg("gRPC server stopped gracefully.")
